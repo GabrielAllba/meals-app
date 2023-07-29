afe33bd0fb29378b251f2e10406927fed3f3601e
rielgbrl <rielgasy9@gmail.com>
2023-07-29 12:00:52 +0700

    add meal screen details

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,7 +1,21 @@
+import 'package:meals/models/category.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+
+void _selectCategory(BuildContext context, Category category) {
+  final filteredMeals = dummyMeals
+      .where((meal) => meal.categories.contains(category.id))
+      .toList();
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (ctx) =>
+          MealsScreen(title: category.title, meals: filteredMeals),
+    ),
+  );
+}
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -22,7 +36,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem(
+      {super.key, required this.category, required this.onSelectCategory});
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
